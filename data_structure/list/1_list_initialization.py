ddb405dffc866a8af5db5e0312c7b322a11a5f35
PYAE PHYO <pyaephyolin.mail@gmail.com>
2025-06-15 20:31:02 +0630

    python list

---
+++ data_structure/list/1_list_initialization.py
@@ -0,0 +1,13 @@
+# List Starting
+
+#Empty List
+empty_list =[]
+print(f"A empty list : {empty_list}, {type(empty_list)}")
+
+string_list = ["python","coffee","technolgy"]   #string list
+number_list = [0,7,9,5]    #numbers list
+mix_list = ["apple", 10, 3.14, None, True] #in a python list can include string, int, float, boolean
+
+print(f"String List : {string_list}.")
+print(f"Numbers List : {number_list}.")
+print(f"Mixed List : {mix_list}.")
